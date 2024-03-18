36b1e82030f076a849a8e2139e047316da2d1b55
Dagmar Kuiper <dkuiper@sogyo.nl>
2024-03-18 12:00:50 +0100

    added card_ID to card class
    Former-commit-id: 3e853a32d2c1cf6d241507c4de898fa978a42958

--- domain/src/main/java/nl/sogyo/Card.java
+++ domain/src/main/java/nl/sogyo/Card.java
@@ -2,5 +2,13 @@ package nl.sogyo;
 
 public abstract class Card {
     private String cardName;
+    private String card_ID;
 
+    public String getCardName() {
+        return cardName;
+    }
+
+    public String getCard_ID() {
+        return card_ID;
+    }
 }
