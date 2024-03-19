fc3f20c0e956283eb29a45d4a3251bd4baefcd60
Dagmar Kuiper <dkuiper@sogyo.nl>
2024-03-19 16:30:19 +0100

    Update cardDTO en gameDTO
    Former-commit-id: 4716b9472fea50a4b9df0e24445074a946eb2981

--- api/src/main/java/gwent/api/models/CardDTO.java
+++ api/src/main/java/gwent/api/models/CardDTO.java
@@ -1,11 +1,12 @@
 package gwent.api.models;
 
 public class CardDTO {
-
     private String cardName;
+    private String cardID;
 
-    public CardDTO(String cardName) {
+    public CardDTO(String cardName, String cardID) {
         this.cardName = cardName;
+        this.cardID = cardID;
     }
 
     public String getCardName() {
--- api/src/main/java/gwent/api/models/GameDTO.java
+++ api/src/main/java/gwent/api/models/GameDTO.java
@@ -8,7 +8,7 @@ public class GameDTO {
     public GameDTO(IGwent gwent) {
         cards = new CardDTO[gwent.getHand().numberOfCards()];
         for(int i = 0; i < gwent.getHand().numberOfCards(); i++) {
-            cards[i] = new CardDTO(gwent.getHand().getCard(i).getCardName());
+            cards[i] = new CardDTO(gwent.getHand().getCard(i).getCardName(), gwent.getHand().getCard(i).getCardID());
         }
     }
 };
