47e23d87b0feccbaa9fb4b03802bea91f71e2292
Dagmar Kuiper <dkuiper@sogyo.nl>
2024-03-20 10:35:11 +0100

    added Player class which creates a new Hand at initialization
    Former-commit-id: b23397f57545fe9dafd69891c181e3e7e0aa75e2

---
+++ domain/src/main/java/gwent/domain/Player.java
@@ -0,0 +1,9 @@
+package gwent.domain;
+
+public class Player {
+    protected Hand hand;
+
+    public Player() {
+        this.hand = new Hand();
+    }
+}
--- domain/src/test/java/gwent/domain/TestPlayer.java
+++ domain/src/test/java/gwent/domain/TestPlayer.java
@@ -1,4 +1,13 @@
 package gwent.domain;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TestPlayer {
+    @Test
+    public void TestPlayerHasHandAfterInitiation() {
+        Player testPlayer = new Player();
+        assertNotNull(testPlayer.hand);
+    }
 }
