b3e66396d84f3d172f963124c962679ff50280cb
Dagmar Kuiper <dkuiper@sogyo.nl>
2024-03-20 10:38:55 +0100

    added HandDTO with list of cards
    Former-commit-id: 9a679f2eac4604f5e5acd6b61a8adb0541bce5d7

---
+++ api/src/main/java/gwent/api/models/HandDTO.java
@@ -0,0 +1,14 @@
+package gwent.api.models;
+
+import gwent.domain.IGwent;
+
+public class HandDTO {
+    public CardDTO[] cards;
+
+    public HandDTO(IGwent gwent) {
+        cards = new CardDTO[gwent.getHand().numberOfCards()];
+        for(int i = 0; i < gwent.getHand().numberOfCards(); i++) {
+            cards[i] = new CardDTO(gwent.getHand().getCard(i).getCardName(), gwent.getHand().getCard(i).getCardID());
+        }
+    }
+}
