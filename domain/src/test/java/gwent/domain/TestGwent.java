1fc438563810e7b6ffdfea2d22fed6a46d4351fd
dkuiper <dkuiper@sogyo.nl>
2024-03-21 15:46:54 +0100

    Added missing tests for adapter
    Former-commit-id: d774cc47ef23744b810b5e4717b57a71e75c8ad3

--- domain/src/test/java/gwent/domain/TestGwent.java
+++ domain/src/test/java/gwent/domain/TestGwent.java
@@ -32,5 +32,28 @@ public class TestGwent {
         assertEquals(Hand.class, testGwent.getPlayer2().getHand().getClass());
     }
 
+    @Test
+    public void TestGetScoreReturnsCorrectScoreAfterPlayingTwoCards() {
+        Gwent testGwent = new Gwent();
+        UnitCard testcard = new UnitCard("testcard", 10);
+        testGwent.getPlayer2().getHand().addCard(testcard);
+        testGwent.playCard(testcard.getCardID(), 2);
+        UnitCard testcard2 = new UnitCard("testcard2", 5);
+        testGwent.getPlayer2().getHand().addCard(testcard2);
+        testGwent.playCard(testcard2.getCardID(), 2);
+        assertEquals(15, testGwent.getScore("close", 2));
+    }
 
+    //TODO test total score when multiple ranges are added
+    @Test
+    public void TestGetTotalScoreReturnsCorrectScoreAfterPlayingTwoCards() {
+        Gwent testGwent = new Gwent();
+        UnitCard testcard = new UnitCard("testcard", 10);
+        testGwent.getPlayer2().getHand().addCard(testcard);
+        testGwent.playCard(testcard.getCardID(), 2);
+        UnitCard testcard2 = new UnitCard("testcard2", 5);
+        testGwent.getPlayer2().getHand().addCard(testcard2);
+        testGwent.playCard(testcard2.getCardID(), 2);
+        assertEquals(15, testGwent.getTotalScore( 2));
+    }
 }
