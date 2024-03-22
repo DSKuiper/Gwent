410825727b26462258f3654b1bde523ac4e361f4
Dagmar Kuiper <dkuiper@sogyo.nl>
2024-03-22 10:39:05 +0100

    Added unit card and special card lists in deck builder that shuffles the bank cards and adds 25 unit cards and 8 special cards to the deck
    Former-commit-id: 65d30af6d51a442ff4e12261bb02ddf20f05227c

--- domain/src/main/java/gwent/domain/DeckBuilder.java
+++ domain/src/main/java/gwent/domain/DeckBuilder.java
@@ -1,6 +1,31 @@
 package gwent.domain;
 
-public class DeckBuilder {
+import java.util.ArrayList;
 
+import static java.util.Collections.shuffle;
+
+public class DeckBuilder {
+    protected ArrayList<Card> unitCards = new ArrayList<>();
+    protected ArrayList<Card> specialCards = new ArrayList<>();
+
+    public DeckBuilder(CardBank bank) {
+        ArrayList<Card> tempUnitCards = new ArrayList<>();
+        tempUnitCards.addAll(bank.northernRealm);
+        tempUnitCards.addAll(bank.northernRealm);
+        tempUnitCards.addAll(bank.neutral);
+        shuffle(tempUnitCards);
+        for(int i = 0; i < 25; i++) {
+            unitCards.add(tempUnitCards.get(i));
+        }
+
+        ArrayList<Card> tempSpecialCards = new ArrayList<>();
+        while(tempSpecialCards.size() < 8) {
+            tempSpecialCards.addAll(bank.special);
+        }
+        shuffle(tempSpecialCards);
+        for(int i = 0; i < 8; i++) {
+            specialCards.add(tempSpecialCards.get(i));
+        }
+    }
 
 }
